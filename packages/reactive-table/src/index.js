db4eba964a40def038a66480a630b030ccb7af42
Sudhanshu <sudhanshu@settlin.in>
2017-06-22 19:12:42 +0530

    fix publish

--- packages/reactive-table/src/index.js
+++ packages/reactive-table/src/index.js
@@ -5,12 +5,39 @@ const ReactiveTable = {};
 import { Meteor } from 'meteor/meteor';
 import PropTypes from 'prop-types';
 
+if (Meteor.isServer) {
+	// publish function
+	ReactiveTable.publish = function(publication, collection, selector = {}, settings = {}) {
+		Meteor.publish('__reactive-table-' + publication, function({publicationId, filters = {}, options = {}}) {
+			check(publicationId, String);
+			check(filters, Object);
+			check(options, {skip: Match.Integer, limit: Match.Integer, sort: Object});
+
+			if (typeof collection === 'function') collection = collection.call(this);
+			if (typeof selector === 'function') selector = selector.call(this);
+
+			if (!(collection instanceof Mongo.Collection)) {
+				console.log('ReactiveTable.publish: no collection to publish'); // eslint-disable-line no-console
+				return [];
+			}
+
+			const filterQuery = {...filters, ...selector};
+			if ((settings || {}).fields) options.fields = settings.fields;
+
+			return [
+				new Counter('count-' + publication + '-' + publicationId, collection.find(filterQuery, {fields: {_id: 1}})),
+				collection.find(filterQuery, options)
+			];
+		});
+	};
+}
+
+// Table components
 class Table extends React.PureComponent {
 	render() {
 		return <div>{this.props.children}</div>;
 	}
 }
-
 let TableContainer;
 let _pubs = {};
 if (Meteor.isClient) {
@@ -35,8 +62,12 @@ if (Meteor.isClient) {
 	}, Table);
 }
 else TableContainer = Table;
-
 class TableWrapper extends React.PureComponent {
+	constructor(props) {
+		super(props);
+		const {publication, collection, selector, settings} = props;
+		if (Meteor.isServer) ReactiveTable.publish(publication, collection, selector, settings);
+	}
 	render() {
 		return <TableContainer {...this.props}/>;
 	}
@@ -54,29 +85,5 @@ TableWrapper.defaultProps = {
 };
 
 ReactiveTable.Component = TableWrapper;
-
-ReactiveTable.publish = function(publication, collection, selector = {}, settings = {}) {
-	Meteor.publish('__reactive-table-' + publication, function({publicationId, filters = {}, options = {}}) {
-		check(publicationId, String);
-		check(filters, Object);
-		check(options, {skip: Match.Integer, limit: Match.Integer, sort: Object});
-
-		if (typeof collection === 'function') collection = collection.call(this);
-		if (typeof selector === 'function') selector = selector.call(this);
-
-		if (!(collection instanceof Mongo.Collection)) {
-			console.log('ReactiveTable.publish: no collection to publish'); // eslint-disable-line no-console
-			return [];
-		}
-
-		const filterQuery = {...filters, ...selector};
-		if ((settings || {}).fields) options.fields = settings.fields;
-
-		return [
-			new Counter('count-' + publication + '-' + publicationId, collection.find(filterQuery, {fields: {_id: 1}})),
-			collection.find(filterQuery, options)
-		];
-	});
-};
 
 export { ReactiveTable };
