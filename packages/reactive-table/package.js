9f7cb9dd3a4613345bda8ad2b1aea37083c8ba0a
Sudhanshu <sudhansh@gmail.com>
2017-11-11 13:33:42 +0530

    update packages

--- packages/reactive-table/package.js
+++ packages/reactive-table/package.js
@@ -1,18 +1,18 @@
 Npm.depends({
-	'react': '15.6.1',
-	'react-dom': '15.6.1',
+	'react': '15.6.2',
+	'react-dom': '15.6.2',
 });
 
 Package.describe({
 	summary: 'A reactive table designed for Meteor + React',
-	version: '1.0.3',
+	version: '1.0.4',
 	name: 'settlin:reactive-table',
 	git: 'https://github.com/settlin/meteor-monorepo.git'
 });
 
 Package.on_use(function(api) {
 	api.versionsFrom('METEOR@1.4.4.1');
-	api.use(['ecmascript', 'mongo', 'react-meteor-data@0.2.12', 'natestrauser:publish-performant-counts@0.1.2']);
+	api.use(['ecmascript', 'mongo', 'react-meteor-data@0.2.15', 'natestrauser:publish-performant-counts@0.1.2']);
 	api.use('check', 'server');
 
 	api.mainModule('src/index.js', ['server', 'client']);
