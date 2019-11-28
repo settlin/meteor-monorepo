e1c081d81cc09494ae1a4895d84da30a144f3bf0
Sudhanshu <sudhanshu@settlin.in>
2019-11-28 14:10:10 +0530

    remove contraints from meteor packages in

--- packages/reactive-table/package.js
+++ packages/reactive-table/package.js
@@ -1,13 +1,13 @@
 Package.describe({
 	summary: 'A reactive table designed for Meteor + React',
-	version: '1.1.5',
+	version: '1.1.6',
 	name: 'settlin:reactive-table',
 	git: 'https://github.com/settlin/meteor-monorepo.git'
 });
 
 Package.on_use(function(api) {
 	api.versionsFrom('METEOR@1.4.4.1');
-	api.use(['ecmascript', 'mongo', 'react-meteor-data@0.2.15', 'natestrauser:publish-performant-counts@0.1.2', 'tmeasday:check-npm-versions@0.3.2']);
+	api.use(['ecmascript', 'mongo', 'react-meteor-data', 'natestrauser:publish-performant-counts', 'tmeasday:check-npm-versions']);
 	api.use('check', 'server');
 
 	api.mainModule('src/index.js', ['server', 'client'], {lazy: true});
