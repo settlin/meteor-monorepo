492eb1e8aedc30ed224575b5f4308679a57c4438
Sudhanshu <sudhanshu@settlin.in>
2022-09-13 17:43:09 +0530

    fix: fixes

--- packages/accounts-zoho/package.js
+++ packages/accounts-zoho/package.js
@@ -1,11 +1,11 @@
 Package.describe({
 	name: 'settlin:accounts-zoho',
   summary: "Login service for Zoho accounts",
-  version: "1.0.0",
+  version: "1.0.1",
 });
 
 Package.onUse(api => {
-  api.versionsFrom('1.4');
+  api.versionsFrom(['1.4', '2.3']);
 	
 	api.use(['ecmascript']);
   api.use('accounts-base', ['client', 'server']);
