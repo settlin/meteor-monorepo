d0f4be95fe6e2b0093068401faafb8cca76a85fd
Sudhanshu <sudhanshu@settlin.in>
2023-12-26 18:34:23 +0530

    fix: fixes

--- packages/accounts-zoho/server.js
+++ packages/accounts-zoho/server.js
@@ -96,7 +96,7 @@ const getAccessToken = async(query, callback) => {
 		callback('No accountId found');
 		throw new Meteor.Error(response.status, `Failed to complete OAuth handshake with zoho. No accountId in zoho response`, {response: data, options, query});
 	}
-	res = {...res, accountId: data[0].accountId}
+	res = {...res, accountId: data[0].accountId};
 
 	// eslint-disable-next-line no-undefined
 	callback(undefined, res);
